f20e53515d2d6decaff9f13455e2e8b46ab96d5b
Yan Zhulanow <Yan.Zhulanow@jetbrains.com>
2017-05-04 14:20:44 +0300

    Set API version requirement (2.0)
    Marta now requires plugins to specify the target API version explicitly.
    This commit sets the target version to the latest API version available.

--- OpenerPlugin/plugin.swift
+++ OpenerPlugin/plugin.swift
@@ -6,6 +6,7 @@ public class NetExistentialtypeOpenerPlugin : NSObject, Plugin, ActionProvider {
   public let name = "opener"
   public let author = "Geoffrey Washburn"
   public let email = "washburn@acm.org"
+  public let requiredApiVersion = ApiVersion.forMartaVersion(release: 0, major: 2)
   
   public var actions: [Action] {
     return [ NetExistentialtypeOpenerAction() ]
